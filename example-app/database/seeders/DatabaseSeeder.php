006efe316353ed6e5a2d7872373a151c8588b239
Santiago Palacio <santiagopalacio@MacBook-Pro-de-Santiago.local>
2024-06-12 13:18:17 -0300

    fix: seeder restored to normal

--- example-app/database/seeders/DatabaseSeeder.php
+++ example-app/database/seeders/DatabaseSeeder.php
@@ -13,9 +13,6 @@ class FlightControllerTest extends TestCase
 {
     use RefreshDatabase;
 
-    private $airline;
-    private $cities;
-
     protected function setUp(): void
     {
         parent::setUp();
@@ -25,78 +22,16 @@ class FlightControllerTest extends TestCase
 
     protected function seedDatabase()
     {
-        $this->cities = City::factory()->count(2)->create();
+        City::factory()->count(4)->create();
 
-        $this->airline = Airline::factory()->count(1)->create();
+        Airline::factory()->count(12)->create();
 
-        $this->airline->cities()->attach($this->cities[0]->id);
-        $this->airline->cities()->attach($this->cities[1]->id);
+        Airline::all()->each(function ($airline) {
+            $cities = City::inRandomOrder()->take(8)->pluck('id');
+            $airline->cities()->attach($cities);
+        });
+
+        Flight::factory()->count(40)->create();
     }
 
-    /**
-     * Test to store a new flight.
-     *
-     * @return void
-     */
-    public function test_store_flight()
-    {
-
-        $data = [
-            'airline_id' => $this->airline->id,
-            'departure_city_id' => $this->cities[0]->id,
-            'arrival_city_id' => $this->cities[1]->id,
-            'departure_datetime' => now()->addHour()->toDateTimeString(),
-            'arrival_datetime' => now()->addHours(2)->toDateTimeString(),
-        ];
-
-        $response = $this->postJson('/api/flights', $data);
-
-        $response->assertStatus(JsonResponse::HTTP_CREATED);
-        $response->assertJsonStructure([
-            'flight' => [
-                'id',
-                'airline_id',
-                'departure_city_id',
-                'arrival_city_id',
-                'departure_datetime',
-                'arrival_datetime',
-                'created_at',
-                'updated_at'
-            ]
-        ]);
-
-        $this->assertDatabaseHas('flights', [
-            'airline_id' => $data['airline_id'],
-            'departure_city_id' => $data['departure_city_id'],
-            'arrival_city_id' => $data['arrival_city_id'],
-            'departure_datetime' => $data['departure_datetime'],
-            'arrival_datetime' => $data['arrival_datetime'],
-        ]);
-    }
-
-    /**
-     * Test to delete a flight.
-     *
-     * @return void
-     */
-    public function test_destroy_flight()
-    {
-        $data = [
-            'airline_id' => $this->airline->id,
-            'departure_city_id' => $this->cities[0]->id,
-            'arrival_city_id' => $this->cities[1]->id,
-            'departure_datetime' => now()->addHour()->toDateTimeString(),
-            'arrival_datetime' => now()->addHours(2)->toDateTimeString(),
-        ];
-
-        $flight = $this->postJson('/api/flights', $data);
-
-        $response = $this->deleteJson('/api/flights/' . $flight->id);
-
-        $response->assertStatus(JsonResponse::HTTP_NO_CONTENT);
-
-        $this->assertDatabaseMissing('flights', [
-            'id' => $flight->id,
-        ]);
-    }
 }
